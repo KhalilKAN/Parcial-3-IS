bc163f5df87329d5d1524ffb97612f7f751e17fc
KhalilKAN <kanasir@hotmail.como>
2021-06-17 10:18:19 -0300

    First commit

---
+++ test/calculadora-tests.js
@@ -0,0 +1,10 @@
+const assert = require("chai").assert;
+const calculadora = require("../calculadora");
+
+const result = calculadora.suma(1,2);
+
+describe("Test Calculadora", function(){
+    it("Test funcion suma", function(){
+        assert.equal(result, 3);
+    });
+});
