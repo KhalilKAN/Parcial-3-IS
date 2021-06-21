438f9599daaae13acd9db8c89950fba6c0d8bc2b
KhalilKAN <kanasir@hotmail.como>
2021-06-20 21:09:52 -0300

    Prueba

--- calculadora.js
+++ calculadora.js
@@ -4,4 +4,4 @@ module.exports = {
     }
 }
 
-document.write(suma(1,2));
+document.write(suma(1,3));
--- test/calculadora-tests.js
+++ test/calculadora-tests.js
@@ -1,11 +1,11 @@
 const assert = require("chai").assert;
 const calculadora = require("../calculadora");
 
-const result = calculadora.suma(1,2);
+const result = calculadora.suma(1,3);
 
 describe("Test Calculadora", function(){
     it("Test funcion suma", function(){
-        assert.equal(result, 3);
+        assert.equal(result, 4);
     });
 });
 
