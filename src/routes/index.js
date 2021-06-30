def35bce62e7c728b9076d9a6e2fc65cd0d710c5
KhalilKAN <kanasir@hotmail.como>
2021-06-29 21:35:03 -0300

    cambios

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const suma = require('../index');
-const router = express();
-
-router.get('/', function(req, res){
-    document.write(suma.suma(1,2));
-});
-
-module.exports = router;
