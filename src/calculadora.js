c62d1c099ef4b4daa43ce0a28d20ef23e3186d35
KhalilKAN <kanasir@hotmail.como>
2021-06-30 16:32:29 -0300

    prueba

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,10 +2,5 @@ function suma(a, b){
     return a + b;
 }
 
-// var arreglo = [1,2,3,4,5];
-// for(var i = 0; i < arreglo.length; i++){
-//     console.log(arreglo[i]);
-// }
-
 
 module.exports = {suma};
