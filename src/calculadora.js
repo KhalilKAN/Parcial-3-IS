acb3884be5887cf3f79256e8b693c6e02af18493
KhalilKAN <kanasir@hotmail.como>
2021-06-30 14:03:08 -0300

    cambios

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,5 +2,9 @@ function suma(a, b){
     return a+b;
 }
 
+// var arreglo = [1,2,3,4,5];
+// for(var i = 0; i < arreglo.length; i++){
+//     console.log(arreglo[i]);
+// }
 
 module.exports = {suma};
