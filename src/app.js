5a354fa046fcdc3ad9885e50ecbe6426e4a639ae
KhalilKAN <kanasir@hotmail.como>
2021-06-29 21:31:15 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
-// const express = require("express");
-// const app = express();
-// const suma = require('./calculadora').suma;
-
-// app.get('/', function(req, res){
-//     res.send(String(suma(1,2)));
-// }); 
-
-// module.exports = app;
-
-
 let express = require('express');
 
-let app1 = express();  // Compliant
+let app1 = express();
 app1.disable("x-powered-by");
 
 const suma = require('./calculadora').suma;
 
 let helmet = require("helmet");
-let app2 = express(); // Compliant
+let app2 = express()
 app2.use(helmet.hidePoweredBy());
 
 app2.get('/', function(req, res){
